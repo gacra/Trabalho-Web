de418fc96c96bbbfedebe99cbe42636aab95eaee
GutoSantos <atsantos@ymail.com>
2017-11-16 15:55:43 -0200

    serializer

--- BackEnd/site_receitas/forms.py
+++ BackEnd/site_receitas/forms.py
@@ -1,1 +1,17 @@
+from django.forms import ModelForm
+from .models import *
 
+class UsuarioForm(ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ('nome', 'nascimento', 'cidade', 'estado', 'telefone', 'usuario')
+
+class IngredienteForm(ModelForm):
+    class Meta:
+        model = Ingrediente
+        fields = ('nome_ingrediente', )
+
+class ImagemReceitaForm(ModelForm):
+    class Meta:
+        model = ImagemReceita
+        fields = ('imagem', 'descricao_imagem',)
