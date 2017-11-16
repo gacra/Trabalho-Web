45aa928ae5e640cb0f4018b91f99c530b94a4c8a
GutoSantos <atsantos@ymail.com>
2017-11-15 22:27:44 -0200

    modelos, forms e teste index.html com forms

--- BackEnd/site_receitas/models.py
+++ BackEnd/site_receitas/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.forms import ModelForm
 from .managers import UserManager
+
+
 
 # Create your models here.
 
@@ -26,3 +29,33 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
 
     def get_short_name(self):
         return self.nome
+
+class UsuarioForm(ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ('nome', 'nascimento', 'cidade', 'estado', 'telefone', 'usuario')
+
+class DescricaoReceita(models.Model):
+    nome_usuario = models.CharField(max_length=100)
+    nome_receita = models.CharField(max_length=100)
+    descrição_receita = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    porcoes = models.IntegerField()
+    calorias = models.IntegerField()
+    tempo_preparo = models.TimeField()
+    preparo = models.CharField(max_length=500)
+    coximento = models.CharField(max_length=500)
+
+class Ingrediente(models.Model):
+    nome_ingrediente = models.CharField(max_length=100)
+
+class IngredienteForm(ModelForm):
+    class Meta:
+        model = Ingrediente
+        fields = ('nome_ingrediente', )
+
+class Receita(models.Model):
+    fk_descricaoReceita = models.ForeignKey('DescricaoReceita', on_delete=models.CASCADE)
+    fk_ingrediente = models.ForeignKey('Ingrediente', on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    unidade = models.CharField(max_length=20)
