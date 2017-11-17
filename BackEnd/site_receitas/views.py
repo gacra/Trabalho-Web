dc2aab62fe8276f736fa756e868f2ee9052182fa
GutoSantos <atsantos@ymail.com>
2017-11-17 17:07:43 -0200

    acho que agora foi

--- BackEnd/site_receitas/views.py
+++ BackEnd/site_receitas/views.py
@@ -1,18 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from rest_framework.renderers import TemplateHTMLRenderer
+
+
 from .models import *
-from .forms import *
+from .serializers import *
 
 # Create your views here.
-def index(request):
+class Home(APIView):
 
-    form = UsuarioForm()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
 
-    return render(request, 'index.html', {'form': form} )
+    def get(self, request, format=None):
+        return Response()
 
-def cadastroUsuario(request):
 
-    return render(request, 'cadastroUsuario.html')
+class CadastroUsuario(APIView):
 
-def cadastroReceita(request):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'cadastroUsuario.html'
 
-    return render(request, 'cadastroReceita.html')
+    def get(self, request):
+        return Response()
+
+    def post(self, request, format=None):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CadastroReceita(APIView):
+
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'cadastroReceita.html'
+
+    def get(self, request, format=None):
+        return Response()
